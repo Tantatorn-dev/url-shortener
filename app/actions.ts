191984dec82a6f21dc0ba097510dca49f088bc88
Tantatorn <timefear007@gmail.com>
2024-06-10 22:00:04 +0700

    ✨ add simple shorten action

---
+++ app/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { createClient } from "redis";
+import { randomBytes } from "crypto";
+
+export async function shorten(formData: FormData) {
+  const url = formData.get("url");
+
+  if (!url || typeof url !== "string") {
+	throw new Error("Invalid URL");
+  }
+
+  const client = createClient();
+
+  await client.connect();
+
+  const key = randomBytes(5).toString("hex")
+
+  await client.set(key, url);
+
+  const value = await client.get(key);
+
+  console.log('saved url:', value);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-export default async function Home() {
+import { shorten } from "./actions";
+
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl mb-10">URL Shortener</h1>
-      <form className="flex flex-row justify-center w-full gap-5">
+      <form action={shorten} className="flex flex-row justify-center w-full gap-5">
         <input
+          name="url"
           type="text"
           placeholder="Type URL here"
           className="input input-bordered w-full max-w-xl"
