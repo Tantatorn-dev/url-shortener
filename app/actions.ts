58aeeb873008bcec393968c220f207e2f57319b3
Tantatorn <timefear007@gmail.com>
2024-06-12 08:18:50 +0700

    ✨ show saved url on UI

--- app/actions.ts
+++ app/actions.ts
@@ -3,19 +3,21 @@
 import { createClient } from "redis";
 import { randomBytes } from "crypto";
 
-export async function shorten(formData: FormData) {
+export async function shorten(
+  currentState: { url: string },
+  formData: FormData
+) {
   const url = formData.get("url");
 
   if (!url || typeof url !== "string") {
-	throw new Error("Invalid URL");
+    throw new Error("Invalid URL");
   }
 
   const client = createClient();
   await client.connect();
 
-  const key = randomBytes(5).toString("hex")
+  const key = randomBytes(5).toString("hex");
   await client.set(key, url);
 
-  const value = await client.get(key);
-  console.log('saved url:', value);
+  return { url: key };
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,39 @@
+'use client';
+
+import { useFormState } from "react-dom";
 import { shorten } from "./actions";
 
 export default function Home() {
+  const [state, formAction] = useFormState(shorten, {url: ""});
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl mb-10">URL Shortener</h1>
-      <form action={shorten} className="flex flex-row justify-center w-full gap-5">
+      <form
+        action={formAction}
+        className="flex flex-row justify-center w-full gap-5"
+      >
         <input
           name="url"
           type="text"
           placeholder="Type URL here"
           className="input input-bordered w-full max-w-xl"
         />
-        <button type="submit" className="btn btn-primary">Shorten</button>
+        <button type="submit" className="btn btn-primary">
+          Shorten
+        </button>
       </form>
+      {state.url && (
+        <div className="mt-10">
+          <a
+            href={`/${state.url}`}
+            className="text-blue-500 underline"
+            target="_blank"
+          >
+            {`/${state.url}`}
+          </a>
+        </div>
+      )}
     </main>
   );
 }
